72584f1791ae51c9b2dfc787787a97e857a105b6
AnastasiaTer-Stepanova <anastasiaterstepanova@gmail.com>
2024-04-22 19:51:09 +0300

    Add openapi java generator, add open-api for genealogy visualize

--- back/controller/build.gradle.kts
+++ back/controller/build.gradle.kts
@@ -1,15 +1,75 @@
+import org.gradle.kotlin.dsl.support.uppercaseFirstChar
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
-    id("java-conventions")
     application
+    id("java-conventions")
+    id("org.openapi.generator") version "7.5.0"
 }
 
 application {
     mainClass.set("genealogy.visualizer.Application")
 }
 
+val yamlFiles = File("$rootDir/openapi/").listFiles()?.filter { file ->
+    file.name.endsWith(".yaml")
+}
+
+val generateDir = "${layout.projectDirectory}/build/generated/openapi"
+val generateJavaOpenApiTasks = yamlFiles?.map { file ->
+    val apiName = file.name.removeSuffix(".yaml").uppercaseFirstChar()
+    tasks.register<GenerateTask>("openApiGenerate$apiName") {
+        group = "openapi tools"
+        generatorName.set("spring")
+        inputSpec.set(file.absolutePath)
+        outputDir.set(generateDir)
+        apiPackage.set("genealogy.visualizer.api")
+        modelPackage.set("genealogy.visualizer.api.model")
+        apiFilesConstrainedTo.set(listOf(""))
+        modelFilesConstrainedTo.set(listOf(""))
+        validateSpec.set(true)
+        additionalProperties.set(
+            mapOf(
+                "interfaceOnly" to "true",
+            )
+        )
+        configOptions.set(
+            mapOf(
+                "library" to "spring-boot",
+                "useTags" to "true",
+                "annotationLibrary" to "swagger2",
+                "skipDefaultInterface" to "true",
+                "generateSupportingFiles" to "false",
+            )
+        )
+    }
+}
+
+
+val generateJavaOpenApi by tasks.registering {
+    group = "openapi tools"
+    dependsOn(generateJavaOpenApiTasks)
+}
+
+tasks.compileJava {
+    dependsOn(generateJavaOpenApi)
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs(File("$generateDir/src/main/java"))
+        }
+    }
+}
 
 dependencies {
     project(":back:service")
 
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.20")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
     implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
 }
---
+++ back/controller/src/main/java/genealogy/visualizer/controller/GenealogyVisualizeController.java
@@ -0,0 +1,39 @@
+package genealogy.visualizer.controller;
+
+import genealogy.visualizer.api.GenealogyVisualizeApi;
+import genealogy.visualizer.api.model.GenealogyVisualizeRs;
+import genealogy.visualizer.api.model.GenealogyVisualizeGraph;
+import genealogy.visualizer.api.model.GenealogyVisualizeRq;
+import genealogy.visualizer.api.model.GraphLinks;
+import genealogy.visualizer.api.model.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
+
+@RestController
+public class GenealogyVisualizeController implements GenealogyVisualizeApi {
+
+    @Override
+    public ResponseEntity<GenealogyVisualizeRs> getGenealogyVisualizeGraph(GenealogyVisualizeRq GenealogyVisualizeRq) {
+        //TODO Временная заглушка
+        GenealogyVisualizeRs GenealogyVisualizeRs = new GenealogyVisualizeRs();
+        GenealogyVisualizeGraph visualizeGraph = new GenealogyVisualizeGraph();
+        for (int i = 0; i < 10; i++) {
+            Person person = new Person();
+            person.setFio(randomAlphabetic(20));
+            person.setId(Integer.valueOf(randomNumeric(3)));
+            visualizeGraph.addPersonsItem(person);
+        }
+        for (int i = 0; i < 4; i++) {
+            GraphLinks links = new GraphLinks();
+            links.setSource(visualizeGraph.getPersons().get(i).getId());
+            links.setTarget(visualizeGraph.getPersons().get(i + 2).getId());
+            visualizeGraph.addLinksItem(links);
+        }
+        GenealogyVisualizeRs.setGenealogyVisualizerGraph(visualizeGraph);
+        return new ResponseEntity<>(GenealogyVisualizeRs, HttpStatus.OK);
+    }
+}
